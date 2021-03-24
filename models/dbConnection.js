a144f03ddfe8f76431be090454977a0f487774d7
vityapopov98 <vityapopov98@yandex.ru>
2021-03-24 15:36:46 +0300

    another DB

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -1,10 +1,18 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv'
 dotenv.config()
-const sequelize = new Sequelize('estrynode', 'root', process.env.PASSWORD,{
-    host: '',
+// const sequelize = new Sequelize('estrynode', 'root', process.env.PASSWORD,{
+//     host: '',
+//     dialect: 'mysql'
+// });
+
+
+//_________Jino_________
+const sequelize = new Sequelize('inventory', 'root', process.env.PASSWORD,{
+    host: 'localhost',
     dialect: 'mysql'
 });
+
 // const sequelize = new Sequelize('heroku_af8a97aef3ea006', 'b98e9436956d35', process.env.PASSWORD,{
 //     host: 'us-cdbr-east-03.cleardb.com',
 //     dialect: 'mysql'
